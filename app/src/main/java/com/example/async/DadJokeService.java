319d02ea94cdcfa069e737ec1f0e55a46578c631
nnnidooo <nadi3252@stud.kea.dk>
2023-03-25 16:52:27 +0400

    DadJokeService async notes

--- app/src/main/java/com/example/async/DadJokeService.java
+++ app/src/main/java/com/example/async/DadJokeService.java
@@ -14,7 +14,7 @@ import java.net.URL;
 
 public class DadJokeService extends AsyncTask <Void,Void,String> {
 
-    WeakReference<TextView> textView;
+    WeakReference<TextView> textView; //weakReference er en type
 
     private String content2;
 
@@ -25,19 +25,29 @@ public class DadJokeService extends AsyncTask <Void,Void,String> {
     }
 
     //A service that fetches a joke from an API and returns data as JSON
+    //Koden forsøger at køre en opgave i en separat tråd, som ikke blokerer hovedtråden.
     @Override
     protected String doInBackground(Void... voids) {
-        StringBuilder content = null;
+        StringBuilder content = null; //StringBuilder er en klasse i Java, der kan bruges til at oprette og manipulere strengdata.
         try{
-            Thread.sleep(2000);
-            URL url = new URL("https://icanhazdadjoke.com/");
+            Thread.sleep(2000); //Tråden sover i 2 sekunder ved hjælp af Thread.sleep(2000).
+            URL url = new URL("https://icanhazdadjoke.com/"); //En URl oprettes.
+
+            //En HTTP-forbindelse oprettes ved hjælp af openConnection()-metoden på URL-objektet.
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+            //Anmodningsmetoden indstilles til "GET" ved hjælp af setRequestMethod()-metoden på forbindelsesobjektet.
             con.setRequestMethod("GET");
+            //Anmodningen accepterer JSON-svar ved hjælp af setRequestProperty()-metoden på forbindelsesobjektet.
             con.setRequestProperty("Accept:", "application/json");
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(con.getInputStream()));
+            //En BufferedReader oprettes ved hjælp af InputStreamReader-klassen, der bruges til at læse data fra inputstrømmen.
+            BufferedReader in = new BufferedReader( //StringBuilder-objektet content oprettes for at gemme den data, der læses fra inputstrømmen.
+                    new InputStreamReader(con.getInputStream())); //Et input strøm oprettes
             String inputLine;
+
             content = new StringBuilder();
+            //En while-loop bruges til at læse og appende hver linje af data fra inputstrømmen til
+            //content ved hjælp af readLine()-metoden på BufferedReader-objektet.
             while ((inputLine = in.readLine()) != null) {
                 content.append(inputLine);
             }
